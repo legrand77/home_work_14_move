9111ac9ced3a3ff48015468fe317b02e9c7eb977
Alexandr Gromov <gromovaleksandr813@gmail.com>
2025-01-22 23:41:41 +0300

    Change_3

--- home_work_14_move_2/home_work_14_move_2.cpp
+++ home_work_14_move_2/home_work_14_move_2.cpp
@@ -13,12 +13,18 @@ private:
 public:
     big_integer(const std::string& s) : cstring(s), cstring_tmp_max(s) {}
 
+    big_integer(big_integer&& other) noexcept // конструктор перемещения
+        : cstring(std::exchange(other.cstring, 0)), 
+        cstring_tmp_max(std::exchange(other.cstring, 0)) {}
+
+
     std::string operator+(const big_integer& other){
+        cstring_add.erase();
         if (cstring.size() > other.cstring.size()) {
             cstring_tmp = other.cstring;
         }
         else {
-            cstring_tmp = std::move(cstring);
+            cstring_tmp = cstring;
             cstring_tmp_max = other.cstring;
         }
          if (cstring.size() != other.cstring.size()) {
@@ -28,6 +34,7 @@ public:
          }
          int k{};
          int z{};
+         int c{};
             for (int i = cstring_tmp_max.size()-1; i > -1; i--) {
                 if (1==k) {
                     z = ((cstring_tmp_max[i]) + (cstring_tmp[i])) - 47;
@@ -39,11 +46,48 @@ public:
                   k = 1;
                 }
 
-              cstring_add.insert(cstring_add.begin(), z);;
+              cstring_add.insert(cstring_add.begin(), z);
+              if ((i == 0) && k == 1) c = 1;
+
              }
 
+            if ( 1 == c) cstring_add.insert(cstring_add.begin(), 49);
         return this->cstring_add;
     }
+
+    std::string operator*(int a) {
+        cstring_tmp = cstring;
+
+        for (int i = 1; i < a; i++) { 
+            int k{};
+            int z{};
+            cstring_add.erase();
+
+            for (int i = cstring.size() - 1; i > -1; i--) {
+                if (1 == k) {
+                    z = ((cstring[i]) + (cstring_tmp[i])) - 47;
+                    k = 0;
+                }
+                else  z = ((cstring[i]) + (cstring_tmp[i])) - 48;
+                if (z > 57) {
+                    z -= 10;
+                    k = 1;
+                }
+                cstring_add.insert(cstring_add.begin(), z);
+            } 
+            cstring_tmp = cstring_add;
+        }
+        
+        return this->cstring_add;
+    }
+    big_integer& operator=(big_integer&& other) noexcept // оператор перемещающего присваивания
+    {
+        std::swap(cstring, other.cstring);
+        std::swap(cstring_tmp_max, other.cstring_tmp_max);
+        return *this;
+    }
+
+
 };
 
 
@@ -51,14 +95,22 @@ public:
 
 int main()
 {
-    auto number1 = big_integer("7165464654654646555555554335342456788935353533354780654357886543");
+    /*auto number1 = big_integer("7165464654654646555555554335342456788935353533354780654357886543");
     auto number2 = big_integer("1100000000554455555555557454353");
     auto result = number1 + number2;
-    std::cout << result << std::endl; 
-    number1 = big_integer("114575");
-    number2 = big_integer("78524");
-    result = number1 + number2;
-    std::cout << result << std::endl; // 193099
-
-    return EXIT_SUCCESS;
+    std::cout << result << std::endl; */
+   auto number1 = big_integer("114575");
+   auto number2 = big_integer("78524");
+   auto  result = number1 + number2;
+   std::cout << result << std::endl; // 193099
+   std::cout << number1*5 << std::endl;
+   auto number3(&number1);
+   auto number4=&number3;
+   result = number2 + number2;
+   std::cout << result << std::endl; 
+   result = number2 + *number3;
+   std::cout << result << std::endl;
+   result = number2 + **number4;
+   std::cout << result << std::endl;
+   return EXIT_SUCCESS;
 }
